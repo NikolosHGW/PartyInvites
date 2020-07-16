44e795a0d5c490370cc0ad9bcd2e39e9e1f55475
Ник Николос <nic23172@gmail.com>
2020-07-16 12:19:03 +0300

    Добавьте файлы проекта.

---
+++ Models/GuestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Введите Ваше имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Введите Ваш email адрес")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Введите Ваш номер телефона")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Выберите вариант")]
+        public bool? WillAttend { get; set; }
+    }
+}
